e3fb599ba3eee69af5a1feb16dd7a69614003d84
Steven Burns <smburns47@gmail.com>
2019-08-12 11:57:56 -0700

    Simple quantum computation

---
+++ simple.py
@@ -0,0 +1,43 @@
+import numpy as np
+
+def ihadamard( N):
+    if N == 1:
+        return np.array( [[1]])
+    elif N == 2:
+        return np.array( [[ 1, 1],
+                          [ 1,-1]])
+    elif N > 2:
+        N2 = N//2
+        assert 2*N2 == N
+        return np.kron( ihadamard( N//2), ihadamard( 2))
+
+def hadamard( N):
+    return ihadamard( N) / np.sqrt(N)
+
+def cnot():
+    return np.array( [[1,0,0,0],
+                      [0,1,0,0],
+                      [0,0,0,1],
+                      [0,0,1,0]])
+
+def sq( v):
+    return np.conj(v)*v
+
+def coord( lst):
+    if len(lst) == 1:
+        return lst[0]
+    else:
+        return lst[-1] + 2*coord( lst[:-1])
+
+def e( N, lst):
+    c = coord(lst)
+    return np.array( [ 1 if i == c else 0 for i in range(N)])
+
+if __name__ == "__main__":
+    simple = np.kron(hadamard(2), np.eye(2)).dot(cnot())
+    print( simple)
+
+    print( sq(simple.dot(e( 4, [0,0]))))
+    print( sq(simple.dot(e( 4, [0,1]))))
+    print( sq(simple.dot(e( 4, [1,0]))))
+    print( sq(simple.dot(e( 4, [1,1]))))
